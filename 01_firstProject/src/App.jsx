461bd6d47b4be7ff7201c6e61b81d28509966517
Abhishek Chaurasia <chaurasiaabhishek98@gmail.com>
2024-03-06 16:19:00 +0530

    add mylist,jsx

--- 01_firstProject/src/App.jsx
+++ 01_firstProject/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Myfiles from "./Myfiles";
+import Mylist from "./Mylist";
 
 function App() {
   return (
     <>
       <h1>React code with Vite | Abhi+</h1>
       <Myfiles />
+      <Mylist></Mylist>
     </>
   );
 }
---
+++ 01_firstProject/src/Mylist.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Mylist() {
+  return (
+    <div>
+      <ol>
+        <li>Name</li>
+        <li>Class</li>
+        <li>Roll no.</li>
+        <li>Address</li>
+        <li>Pin code</li>
+      </ol>
+    </div>
+  );
+}
+
+export default Mylist;
