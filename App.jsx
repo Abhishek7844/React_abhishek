ca46a7d706c8562f0ba0595ec975003416a5f9b7
Abhishek Chaurasia <chaurasiaabhishek98@gmail.com>
2024-03-06 17:45:15 +0530

    saveds

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Myfiles from "./Myfiles";
+import Mylist from "./Mylist";
+import Chalo from "./Chalo";
+
+function App() {
+  return (
+    <>
+      <h1>React code with Vite | Abhi+</h1>
+      <Myfiles />
+      <Mylist></Mylist>
+      <Chalo></Chalo>
+    </>
+  );
+}
+
+export default App;
---
+++ Chalo.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Chalo() {
+  return (
+    <div>
+      <h2>Destinstions:</h2>
+      <ol>
+        <li>Gudgaon </li>
+        <li>Koyambtore</li>
+        <li>Chennai</li>
+        <li>Pune</li>
+        <li>Noida</li>
+        <li>Delhi</li>
+        <li>Delhi</li>
+      </ol>
+    </div>
+  );
+}
+
+export default Chalo;
