1c6f4ec63ff32138031ec2dd8356f4894ce1dc48
Abhishek Chaurasia <chaurasiaabhishek98@gmail.com>
2024-03-06 16:48:47 +0530

    add Chalo.jsx

---
+++ 01_firstProject/src/Chalo.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Chalo() {
+  return (
+    <div>
+      <h2>Destinstions:</h2>
+      <ol>
+        <li>Gudgaon </li>
+        <li>Koyambtore</li>
+        <li>Chennai</li>
+        <li>Pune</li>
+        <li>Noida</li>
+      </ol>
+    </div>
+  );
+}
+
+export default Chalo;
