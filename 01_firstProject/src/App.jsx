42d3c601c1dd612943235bcdc3f78bb23f79a420
Abhishek Chaurasia <chaurasiaabhishek98@gmail.com>
2024-03-06 15:57:39 +0530

    readme

---
+++ 01_firstProject/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Myfiles from "./Myfiles";
+
+function App() {
+  return (
+    <>
+      <h1>React code with Vite | Abhi+</h1>
+      <Myfiles />
+    </>
+  );
+}
+
+export default App;
