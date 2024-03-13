8ab77ff1c025cb69c8c5ead75ac682bf44d11ed3
Abhishek7844 <chaurasiaabhishek98@gmail.com>
2024-03-13 11:46:20 +0530

    new project tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,79 +9,32 @@ function App() {
     <>
       <h1 className="bg-gray-400 text-black rounded-sm">Abhishek</h1>
 
-      <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
         <img
-          class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
+          className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
           src="https://images.pexels.com/photos/20480941/pexels-photo-20480941/free-photo-of-sami-3.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt=""
           width="384"
           height="512"
         />
-        <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
           <blockquote>
-            <p class="text-lg font-medium">
+            <p className="text-lg font-medium">
               “Tailwind CSS is the only framework that I've seen scale on large
               teams. It’s easy to customize, adapts to any design, and the build
               size is tiny.”
             </p>
           </blockquote>
-          <figcaption class="font-medium">
-            <div class="text-sky-500 dark:text-sky-400">Sarah Dayan</div>
-            <div class="text-slate-700 dark:text-slate-500">
+          <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">Sarah Dayan</div>
+            <div className="text-slate-700 dark:text-slate-500">
               Staff Engineer, Algolia
             </div>
           </figcaption>
         </div>
       </figure>
-
-      <div class="w-[300px] rounded-md border mt-6">
-        <img
-          src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
-          alt="Laptop"
-          class="h-[200px] w-full rounded-t-md object-cover"
-        />
-        <div class="p-4">
-          <h1 class="inline-flex items-center text-lg font-semibold">
-            About Macbook  {" "}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="h-4 w-4"
-            >
-              <line x1="7" y1="17" x2="17" y2="7"></line>
-              <polyline points="7 7 17 7 17 17"></polyline>
-            </svg>
-          </h1>
-          <p class="mt-3 text-sm text-gray-600">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-            debitis?
-          </p>
-          <div class="mt-4">
-            <span class="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-              #Macbook
-            </span>
-            <span class="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-              #Apple
-            </span>
-            <span class="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-              #Laptop
-            </span>
-          </div>
-          <button
-            type="button"
-            class="mt-4 w-full rounded-sm bg-black px-2 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-          >
-            Read
-          </button>
-        </div>
-      </div>
+      <Card username="Lappi_luthara" btntxt="Click here.." />
+      <Card username="Gopinath" btntxt="View more.." />
     </>
   );
 }
